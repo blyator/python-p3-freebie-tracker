7d92346877e1c76e5b7cf05b19014396565092e8
Billy Yator <dmnbilly@gmail.com>
2025-05-26 08:27:24 +0300

    finished

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -9,6 +10,8 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+
+
 class Company(Base):
     __tablename__ = 'companies'
 
@@ -16,14 +19,48 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
-    def __repr__(self):
-        return f'<Company {self.name}>'
+
+    freebies = relationship("Freebie", back_populates="company")
+    devs = relationship('Dev', secondary='freebies', back_populates='companies')
+
+    @classmethod
+    def oldest_company(cls, session):
+        return session.query(cls).order_by(cls.founding_year).first()
+
+
+
+
 
 class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    name = Column(String())
 
-    def __repr__(self):
-        return f'<Dev {self.name}>'
+
+    freebies = relationship("Freebie", back_populates="dev")
+    companies = relationship('Company', secondary='freebies', back_populates='devs')
+
+
+    def give(self, other_dev, freebie):
+        if freebie in self.freebies:
+            freebie.dev = other_dev
+
+
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer, primary_key=True)
+    item_name = Column(String, nullable=False)
+    value = Column(Integer, nullable=False)
+    dev_id = Column(Integer, ForeignKey('devs.id'))
+    company_id = Column(Integer, ForeignKey("companies.id"))
+
+    dev = relationship("Dev", back_populates="freebies")
+    company = relationship("Company", back_populates="freebies")
+
+    def print_details(self):
+        return f'{self.dev.name} owns a {self.item_name} from {self.company.name}.'
+
+
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,38 @@
 #!/usr/bin/env python3
 
-# Script goes here!
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Dev, Company, Freebie
+
+
+engine = create_engine('sqlite:///lib/freebies.db')
+
+
+Base.metadata.create_all(engine)
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+
+company1 = Company(name="safaricom", founding_year=2004)
+company2 = Company(name="Google", founding_year=1998)
+
+
+dev1 = Dev(name="Billy")
+dev2 = Dev(name="Kelvin")
+
+
+session.add_all([company1, company2, dev1, dev2])
+session.commit()
+
+
+freebie1 = Freebie(item_name="laptop", value=50000, company=company1, dev=dev1)
+freebie2 = Freebie(item_name="Stickers", value=500, company=company1, dev=dev2)
+
+session.add_all([freebie1, freebie2])
+session.commit()
+
+print("Seed testing complete")
+
